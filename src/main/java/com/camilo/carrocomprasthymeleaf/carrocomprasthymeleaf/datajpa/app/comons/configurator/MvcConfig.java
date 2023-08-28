5294c7fb5ca683f9f082b3dc1f3a370946f3ea03
camiiruiz13 <69661273+camiiruiz13@users.noreply.github.com>
2023-08-28 14:18:39 -0500

    se arega camos para validacion de idioma

--- src/main/java/com/camilo/carrocomprasthymeleaf/carrocomprasthymeleaf/datajpa/app/comons/configurator/MvcConfig.java
+++ src/main/java/com/camilo/carrocomprasthymeleaf/carrocomprasthymeleaf/datajpa/app/comons/configurator/MvcConfig.java
@@ -1,13 +1,19 @@
 package com.camilo.carrocomprasthymeleaf.carrocomprasthymeleaf.datajpa.app.comons.configurator;
 
 
+
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
@@ -22,7 +28,24 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addViewController("/error_403").setViewName("error_403");
     }
 
+    @Bean
+    public LocaleResolver localResolver(){
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(new Locale("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+
+        return localeChangeInterceptor;
+    }
 
 
-
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+       registry.addInterceptor(localeChangeInterceptor());
+    }
 }
